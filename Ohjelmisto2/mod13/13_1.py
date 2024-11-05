e5a14fbbbf49e783f790747c4012901621bc9ec1
Jimias <jimias@metropolia.fi>
2024-11-05 09:40:33 +0200

    First commit, tehtävät lisätty

---
+++ Ohjelmisto2/mod13/13_1.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route('/')
+def alkuluku(luku):
+    if luku > 1:
+        for i in range (2, (luku//2)+1):
+
+            if (luku % i) == 0:
+                return False
+        else:
+            return True
+    else:
+        return False
+
+@app.route('/alkuluku/<int:numero>', methods=['GET'])
+def tarkista(numero):
+    result = {
+        "Number": numero,
+        "isPrime": alkuluku(numero)
+    }
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
+
+
